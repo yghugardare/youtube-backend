13ae2dc16eff9708896f317b580b8c3b0ff9e467
Yash Ghugardare <yghugardare6@gmail.com>
2024-01-13 17:50:33 +0530

    .env bug fixed in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import {app} from './app.js';
 dotenv.config({
-    path: "./env"
+    path: "./.env"
 })
 connectDB()
 .then(()=>{
